0c7068f2e6e2cc6ce66462b64259e45618261e2f
yaromirko <ronanndex@gmail.com>
2022-07-10 02:02:02 +0500

    proportion

--- GenesAnalysis.R
+++ GenesAnalysis.R
@@ -17,3 +17,11 @@ S_height <- 7.5 / 2
 plt <- list()
 
 my.colors = c("#008B8B", "#696969", "#BA55D3", "#679B9B", "#E79C2A", "#F56FAD", "#9C19E0", "#FF9A76", "#637373")
+
+formatter <- function(x) {
+  dplyr::case_when(
+    x < 1e3 ~ as.character(x),
+    x < 1e6 ~ paste0(as.character(x/1e3), "K"),
+    x < 1e9 ~ paste0(as.character(x/1e6), "M")
+  )
+}
