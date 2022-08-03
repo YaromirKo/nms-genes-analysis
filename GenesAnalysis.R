1944034b3d16f47382517b8ab4417b9ea57c45ab
yaromirko <ronanndex@gmail.com>
2022-08-03 21:47:28 +0500

    added correct random seed for parallel computing (multicore)

--- GenesAnalysis.R
+++ GenesAnalysis.R
@@ -2,9 +2,13 @@
 
 require("tidyverse")
 require("readxl")
+require("openxlsx") # for normal write xlsx format
 # require("patchwork")
+require("officer")
+require("rvg")
 require("boot")
 require("boot.pval")
+library("RColorBrewer")
 
 my.cores <- min(10, parallel::detectCores() - 1) # always leave at least one free core to keep system responsive
 Sys.setenv(MAKEFLAGS = paste0("-j", my.cores))
@@ -16,7 +20,8 @@ S_height <- 7.5 / 2
 
 plt <- list()
 
-my.colors = c("#008B8B", "#696969", "#BA55D3", "#679B9B", "#E79C2A", "#F56FAD", "#9C19E0", "#FF9A76", "#637373")
+my.colors = c("#CC6677", "#44AA99", "#637373", "#E79C2A", "#008B8B", "#696969", "#BA55D3", "#679B9B", "#F56FAD", "#9C19E0", "#FF9A76")
+heatmap.colors <- colorRampPalette(brewer.pal(8, "PiYG"))(25)
 
 formatter <- function(x) {
   dplyr::case_when(
